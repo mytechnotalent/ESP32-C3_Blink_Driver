17a7a8434611c2142b4b87f6b49c580f5e873fb0
Kevin Thomas <kevin@mytechnotalent.com>
2025-11-29 18:11:35 -0500

    Fix formatting of jump instruction to .loop

--- src/main.s
+++ src/main.s
@@ -35,5 +35,5 @@ main:
   jal   gpio_toggle                              # call gpio_toggle
   li    a0, 500                                  # 500 ms delay
   jal   delay_ms                                 # call delay_ms
-  j     .loop									                   # jump to .loop
+  j     .loop                                    # jump to .loop
 .size main, .-main
